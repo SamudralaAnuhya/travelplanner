73e92e56a20f703f9c25b0fba45dfd9552aa6015
SamudralaAnuhya <156963541+SamudralaAnuhya@users.noreply.github.com>
2024-04-08 01:33:27 -0400

    map working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import React from "react";
+import React , {useEffect, useState} from "react";
 import "./app.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Components/Home/Home";
 import Main from "./Components/Main/Main";
+
+import { CssBaseline, Grid } from "@material-ui/core";
+import { getPlacesData  } from './api';
+
 import Map from "./Components/Map/Map";
 import List from "./Components/List/List";
+import Header from "./Components/Header/Header";
 // import PlaceDetails from "./Components/PlaceDetails/PlaceDetails";
 // import Footer from "./Components/Footer/Footer";
 
 const App = () => {
+    const [places, setPlaces] = useState([]);
+    const [coordinates, setCoordinates] = useState({});
+    const [bounds, setBounds] = useState(null)
+   
+    
+    // useEffect(() =>{
+    //   navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+    //     setCoordinates({ lat: latitude, lng: longitude });
+    //   });
+    // }, []);
+
+    useEffect(() => {
+      const getLocation = () => {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            setCoordinates({ lat: position.coords.latitude, lng: position.coords.longitude });
+          },
+          (error) => {
+            console.error('Error getting geolocation:', error);
+          }
+        );
+      };
+
+
+      getLocation();
+    }, []);
+    
+    
+    useEffect( ()=>{
+        console.log(coordinates,bounds); 
+       
+        getPlacesData()
+        .then( (data) => {
+            console.log(data);
+            setPlaces(data)
+        });    
+    },[coordinates , bounds]);
+
+
   return (
     <>
       <Navbar />
       <Home />
       <Main />
-      <Map />
-      <List />
       {/* <PlaceDetails /> */}
       {/* <Footer/>   */}
+    <> 
+        <CssBaseline />
+        <Header />
+        <Grid container spacing={3} style={{ width: "100%" }}>
+            <Grid item xs={12} md={4}>
+                 <List />
+            </Grid>
+            <Grid item xs={12} md={8}>
+                <Map 
+                setCoordinates ={setCoordinates}
+                setBounds ={setBounds}
+                coordinates ={coordinates}/>
+
+            </Grid>
+        </Grid>
     </>
+    </>
+   
+  
   );
 };
 
--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -7,11 +7,11 @@ import Rating from '@material-ui/lab/Rating';
 // import mapStyles from '../../mapStyles';
 import useStyles from  "./styles";
 
-const Map = () => {  
+const Map = ({setCoordinates, setBounds , coordinates}) => {  
     const classes = useStyles();
     const isMobile = useMediaQuery('(min-width:600px)');
     
-    const coordinates  = {lat: 0 , lng : 0 };
+    // const coordinates  = {lat:10 , lng : 0 };
 
     return (
         <div className={classes.mapContainer}>
@@ -22,7 +22,12 @@ const Map = () => {
                 defaultZoom={14}
                 margin={[50, 50, 50, 50]}
                 options={ '' }
-                onChange={ ''}
+                onChange={(e) =>  {
+                    console.log(e)
+                        setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+                        setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+                      
+                } }
                 onChildClick={''}
             >
             </GoogleMapReact>
